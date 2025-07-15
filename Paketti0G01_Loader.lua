453216d01401e8112bdae014866f8a4a5c2e148b
esaruoho <esaruoho@gmail.com>
2025-07-15 22:02:27 +0300

    bpm pitch fix

--- Paketti0G01_Loader.lua
+++ Paketti0G01_Loader.lua
@@ -79,6 +79,7 @@ function create_device_entry(name, path, device_type)
 end
 
 preferences = renoise.Document.create("ScriptingToolPreferences") {
+  singlewaveformwriterhex=true,
   paketti_auto_hide_disk_browser = false,
   pakettiRePitchEnhanced = false,
   PakettiSteppersGlobalStepCount="16",
