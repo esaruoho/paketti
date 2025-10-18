cadd08085efd5126f6080bf2d6ba984b6047968c
esaruoho <esaruoho@gmail.com>
2025-10-18 18:19:04 +0200

    file paketti export for 8svx iff 16sv

--- PakettiIFFLoader.lua
+++ PakettiIFFLoader.lua
@@ -1431,8 +1431,10 @@ renoise.tool():add_menu_entry{name = "Sample Editor:Paketti:Export:Save Current 
 renoise.tool():add_menu_entry{name = "Sample Editor:Paketti:Export:Save Current Sample as 16SV...",invoke = saveCurrentSampleAs16SV}
 renoise.tool():add_menu_entry{name = "--Sample Editor:Paketti:Export:Batch Convert WAV/AIFF to 8SVX...",invoke = batchConvertToIFF}
 renoise.tool():add_menu_entry{name = "Sample Editor:Paketti:Export:Batch Convert WAV/AIFF to 16SV...",invoke = batchConvertTo16SV}
-renoise.tool():add_menu_entry{name = "--Main Menu:File:Paketti:Load IFF Sample File...",invoke = loadIFFSampleFromDialog}
-renoise.tool():add_menu_entry{name = "Main Menu:File:Paketti:Export:Convert IFF to WAV...",invoke = convertIFFToWAV}
-renoise.tool():add_menu_entry{name = "Main Menu:File:Paketti:Export:Convert WAV to IFF...",invoke = convertWAVToIFF}
-renoise.tool():add_menu_entry{name = "--Main Menu:File:Paketti:Export:Batch Convert WAV/AIFF to 8SVX...",invoke = batchConvertToIFF}
-renoise.tool():add_menu_entry{name = "Main Menu:File:Paketti:Export:Batch Convert WAV/AIFF to 16SV...",invoke = batchConvertTo16SV}
+
+
+renoise.tool():add_menu_entry{name = "--Main Menu:File:Paketti Export:Load IFF Sample File...",invoke = loadIFFSampleFromDialog}
+renoise.tool():add_menu_entry{name = "Main Menu:File:Paketti Export:Convert IFF to WAV...",invoke = convertIFFToWAV}
+renoise.tool():add_menu_entry{name = "Main Menu:File:Paketti Export:Convert WAV to IFF...",invoke = convertWAVToIFF}
+renoise.tool():add_menu_entry{name = "--Main Menu:File:Paketti Export:Batch Convert WAV/AIFF to 8SVX...",invoke = batchConvertToIFF}
+renoise.tool():add_menu_entry{name = "Main Menu:File:Paketti Export:Batch Convert WAV/AIFF to 16SV...",invoke = batchConvertTo16SV}
